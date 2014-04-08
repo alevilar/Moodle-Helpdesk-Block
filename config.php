02b83e9e0a499116616283b65a829564fb6a8cc2
alevilar <alevilar@gmail.com>
2014-04-08 12:22:23 -0300

    config bug solved

--- config.php
+++ config.php
@@ -1,10 +1,6 @@
 <?php
 
-// HACK TEMPORAL PARA DESARROLLAR YO: AleVilar
-$config = '/var/www/moodle22/config.php';
-
-// este es el correcto
-//$config = dirname(__FILE__) . '/../../config.php';
+$config = dirname(__FILE__) . '/../../config.php';
 require_once($config);
 
 
